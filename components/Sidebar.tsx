828debbd7914a00aebede2b834d6ceee409fa6e6
Adeyemi Fathia <fathiaomolara02@gmail.com>
2025-12-27 21:43:17 +0100

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Sidebar = ({user}: SiderbarProps) => {
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between border-r border-gray-200 bg-white pt-8 text-white max-md:hidden sm:p-4 xl:p-6 2xl:w-[355px]'>
-        <nav className='flex flex-col gap-4'></nav>
+        <nav className='flex flex-col gap-4'>
+            <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+            <Image src="/icons/logo.png" alt="logo" width={34} height={34} className="size-[24px] max-xl:size-14"/>
+            <h1 className="2xl:text-26 font-ibm-plex-serif text-[26px] font-bold text-black-1 max-xl:hidden">Flowpay</h1>
+            </Link>
+
+            {sidebarLinks}
+        </nav>
     </section>
   )
 }
